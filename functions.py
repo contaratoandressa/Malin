97df097278166eb670935c2959f8edebda87dade
Andressa Contarato <51369984+contaratoandressa@users.noreply.github.com>
2024-05-11 00:41:37 -0300

    Update functions.py
    add name == estabelecimentos

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ from os import listdir # listar arquivos
 from os.path import isfile, join # listar arquivos
 
 # base: Fazenda - Empresas
-def dwl_database(mypath = '.../', compl = 'cnae/', name = 'cnae'):
+def dwl_database(mypath = '/content/drive/MyDrive/CONSULTORIAS/UFF - Projeto Niterói/Bases de Dados/bases_fazenda/', compl = 'cnae/', name = 'cnae'):
     """Download databases.
     mypath = list of the path that the sheets are.
     compl = de exatc path.
@@ -27,7 +27,18 @@ def dwl_database(mypath = '.../', compl = 'cnae/', name = 'cnae'):
     if name == 'cnae':
       df = df.rename(columns={0: "Codigo", 1: "Descricao"})
     elif name == 'empresas':
-      df = df.rename(columns={0: "CnpjBasico", 1: "RazaoSocial", 2: "NaturezaJuridica", 4: "QualificacaoResponsavel", 5: "CapitalSocial", 6: "Porte", 
-                              7: "EnteFederativoResponsavel"})
+      df = df.rename(columns={0: "CnpjBasico", 1: "RazaoSocial", 2: "NaturezaJuridica", 3: "QualificacaoResponsavel", 4: "CapitalSocial", 5: "Porte",
+                              6: "EnteFederativoResponsavel"})
+    elif name == 'estabelecimentos':
+      df = df.rename(columns={0: "CnpjBasico", 1: "CnpjOrdem", 2: "CnpjDv", 3: "IdMatrizFilial", 4: "NomeFantasia",
+                              5: "SituacaoCadastral", 6: "DataSituacaoCadastral", 7: "MotivoSituacaoCadastral", 8: "NomeCidadeExterior",
+                              9: "Pais", 10: "DataIniAtv", 11: "CnaeFiscalPrincipal", 12: "CnaeFiscalSecundaria", 13: "TipoLogradouro",
+                              14: "Logradouro", 15: "Numero", 16: "Complemento", 17: "Bairro", 18: "CEP", 19: "UF", 20: "Municipio",
+                              21: "ddd1", 22: "Tel1", 23: "ddd2", 24: "Tel2", 25: "dddFax", 26: "Fax", 27: "CorreioEletronico",
+                              28: "SituacaoEspecial", 29: "DataSituacaoEspecial"})
+
+    # save into drive
+    df.to_csv(mypath+name)
+
     # return
     return(df)
